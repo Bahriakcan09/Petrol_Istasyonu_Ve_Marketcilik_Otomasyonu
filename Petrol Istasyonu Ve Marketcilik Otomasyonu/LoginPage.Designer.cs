4c551d2bb7b13a72f164f8ce71fc0374463dee43
Bahri Akçan <akcan750bahri09@gmail.com>
2023-05-17 14:56:50 +0300

    LoginPage arayüzü tasarlandı.

--- Petrol Istasyonu Ve Marketcilik Otomasyonu/LoginPage.Designer.cs
+++ Petrol Istasyonu Ve Marketcilik Otomasyonu/LoginPage.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.usernameTxt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LoginBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginBtn.Location = new System.Drawing.Point(69, 293);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(315, 45);
+            this.LoginBtn.TabIndex = 14;
+            this.LoginBtn.Text = "Giriş Yap";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.passwordTxt.Location = new System.Drawing.Point(115, 232);
+            this.passwordTxt.MaxLength = 16;
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(268, 22);
+            this.passwordTxt.TabIndex = 13;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu.Properties.Resources.password;
+            this.pictureBox2.Location = new System.Drawing.Point(69, 223);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // usernameTxt
+            // 
+            this.usernameTxt.BackColor = System.Drawing.SystemColors.Window;
+            this.usernameTxt.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usernameTxt.Location = new System.Drawing.Point(115, 176);
+            this.usernameTxt.MaxLength = 50;
+            this.usernameTxt.Multiline = true;
+            this.usernameTxt.Name = "usernameTxt";
+            this.usernameTxt.Size = new System.Drawing.Size(268, 20);
+            this.usernameTxt.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu.Properties.Resources.mail;
+            this.pictureBox1.Location = new System.Drawing.Point(69, 166);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(453, 505);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.usernameTxt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LoginPage";
             this.Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox usernameTxt;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
