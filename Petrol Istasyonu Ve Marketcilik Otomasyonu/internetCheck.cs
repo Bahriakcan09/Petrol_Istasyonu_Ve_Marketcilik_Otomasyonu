d37c1684296ffef849b99831f496ddece75bf7b2
Bahri Akçan <akcan750bahri09@gmail.com>
2023-05-17 14:45:44 +0300

    İnternet kontrolünü sağlamak için class oluşturuldu.

---
+++ Petrol Istasyonu Ve Marketcilik Otomasyonu/internetCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
+{
+    internal class internetCheck
+    {
+        public static bool internet()
+        {
+            WebClient webClient = new WebClient();
+            try
+            {
+                webClient.OpenRead("https://google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
