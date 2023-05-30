58368aee0142fa5279771acf0316508aa10b0cbd
Melih2525 <114140561+Melih2525@users.noreply.github.com>
2023-05-30 19:13:32 +0300

    firebase bağlantısı yapıldı

--- Petrol Istasyonu Ve Marketcilik Otomasyonu/MarketPage.cs
+++ Petrol Istasyonu Ve Marketcilik Otomasyonu/MarketPage.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Firebase.Database;
+using Firebase.Database.Query;
 
 namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
 {
@@ -16,5 +18,50 @@ namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
         {
             InitializeComponent();
         }
+        var auth = "QIcj0RgPDtVzQFW919C9ojVnJkJDff7Ps4m7lqQq"; // your app secret
+        var firebaseClient = new FirebaseClient(
+            "<QIcj0RgPDtVzQFW919C9ojVnJkJDff7Ps4m7lqQq>");
+        //new FirebaseOptions
+        //{
+        //    AuthTokenAsyncFactory = () => Task.FromResult(auth)
+        //}
+
+
+        private void MarketPage_Load(object sender, EventArgs e)
+        {
+             var firebase = new FirebaseClient("https://petrol-ve-marketcilik-default-rtdb.firebaseio.com/");
+             var dinos = await firebase
+                .Child("")
+                .OrderByKey()
+                .StartAt("")
+                .LimitToFirst()
+                .OnceAsync<>();
+  
+             foreach (var dino in dinos)
+             {
+              Console.WriteLine($"{dino.Key} is {dino.Object.Height}m high.");
+             }
+        }
+ 
+             var firebase = new FirebaseClient("https://petrol-ve-marketcilik-default-rtdb.firebaseio.com/");
+   
+             var dino = await firebase
+               .Child("")
+               .PostAsync(new Dinosaur());
+             
+             Console.WriteLine($"Key for the new dinosaur: {dino.Key}");
+             
+             await firebase
+               .Child("dinosaurs")
+               .Child("t-rex")
+               .PutAsync(new Dinosaur());
+             
+             await firebase
+               .Child("dinosaurs")
+               .Child("t-rex")
+               .DeleteAsync();
+                }
+
+        
     }
-}
+
